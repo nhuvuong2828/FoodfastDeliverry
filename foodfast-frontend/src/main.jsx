929722d2f7516d8da2f1b161629668bfd0c0353d
nhuvuong2828 <nhuvuong2828@gmail.com>
2025-12-18 03:02:38 +0700

    Debug Register Page: Print HTML source

--- foodfast-frontend/src/main.jsx
+++ foodfast-frontend/src/main.jsx
@@ -1,19 +1,15 @@
 ﻿// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.jsx' // <--- App chỉ được import ở đây
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-
-// QUAN TRỌNG: Import các Context Provider
-// (Đảm bảo bạn đã có file context, nếu chưa có hãy xem phần dưới)
 import { AuthProvider } from './context/AuthContext'
 import { CartProvider } from './context/CartContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <BrowserRouter>
-            {/* Bọc App trong các Provider để các trang con dùng được useContext */}
             <AuthProvider>
                 <CartProvider>
                     <App />
